47a19ed9afa26452c50637a1eb28fbafa3ed93bb
George Ogata <george.ogata@gmail.com>
2009-09-02 02:20:19 -0400

    Defer creation of email classes until use.
    Prevents a "not missing constant" error when used in an application.

--- lib/easy_mailer.rb
+++ lib/easy_mailer.rb
@@ -1,5 +1,2 @@
 require 'easy_mailer/helper'
 require 'easy_mailer/base'
-
-module EasyMailer
-end
--- lib/easy_mailer/base.rb
+++ lib/easy_mailer/base.rb
@@ -37,23 +37,32 @@ module EasyMailer
       end
     end
 
-    #
-    # Define an email, which will be validated by an email model.
-    #
-    def self.email(name, &block)
-      email_class_name = "#{name.to_s.camelize}Email"
-      email_class = const_set(email_class_name, Class.new(Email))
-      email_class.class_eval(&block)
-      class_eval <<-EOS
-        def self.#{name}_email(attributes)
-          #{email_class_name}.new(self, :#{name}, attributes)
-        end
+    @@undefined_email_classes = {}
+    class << self
+      #
+      # Define an email, which will be validated by an email model.
+      #
+      def email(name, &block)
+        email_class_name = "#{name.to_s.camelize}Email"
+        @@undefined_email_classes[name] = [email_class_name, block]
+        class_eval <<-EOS
+          def self.#{name}_email(attributes)
+            define_email_class(:#{name}) if @@undefined_email_classes[:#{name}]
+            #{email_class_name}.new(self, :#{name}, attributes)
+          end
 
-        def #{name}(email)
-          @email = email
-          recipients email.recipients
-        end
-      EOS
+          def #{name}(email)
+            @email = email
+            recipients email.recipients
+          end
+        EOS
+      end
+
+      def define_email_class(name)
+        name, definition = @@undefined_email_classes.delete(name)
+        klass = const_set(name, Class.new(Email))
+        klass.class_eval(&definition)
+      end
     end
 
     helper EasyMailer::Helper
